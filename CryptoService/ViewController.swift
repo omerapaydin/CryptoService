fcd0d827a8832f7e06c5e4e1e09fae743e952cb1
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-02 17:30:16 +0300

    Initial Commit

---
+++ CryptoService/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoService
+//
+//  Created by Ömer on 2.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CryptoServiceTests/CryptoServiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoServiceTests.swift
+//  CryptoServiceTests
+//
+//  Created by Ömer on 2.03.2025.
+//
+
+import Testing
+@testable import CryptoService
+
+struct CryptoServiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
